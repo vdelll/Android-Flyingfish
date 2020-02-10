7c0cee28be95fcd2589acb04902a6e33b8953a4b
vdelll <valentin.delaunay1@gmail.com>
2020-02-11 00:08:13 +0100

    Création des boules de couleur vertes, jaunes et rouges avec modifiation du score s'il y a collision entre les boules et le poisson

--- app/src/main/java/com/appli/flyingfish/FlyingFishView.java
+++ app/src/main/java/com/appli/flyingfish/FlyingFishView.java
@@ -11,7 +11,7 @@ import android.view.MotionEvent;
 import android.view.View;
 
 /**
- * Vue contenant le poisson.
+ * Vue du jeu.
  */
 
 public class FlyingFishView extends View {
@@ -25,6 +25,19 @@ public class FlyingFishView extends View {
     // Dimensions du canvas
     private int canvasWidth, canvasHeight;
 
+    // Caractéristiques des boules jaunes
+    private int yellowX, yellowY, yellowSpeed = 16;
+    private Paint yellowPaint = new Paint();
+
+    // Caractéristiques des boules vertes
+    private int greenX, greenY, greenSpeed = 20;
+    private Paint greenPaint = new Paint();
+
+    // Caractéristiques des boules vertes
+    private int redX, redY, redSpeed = 25;
+    private Paint redPaint = new Paint();
+
+    // Booleen pour le toucher de l'écran
     private boolean touch = false;
 
     // fond d'écran
@@ -36,13 +49,20 @@ public class FlyingFishView extends View {
     // Coeurs de vie
     private Bitmap life[] = new Bitmap[2];
 
+    // score
+    private int score;
+
     /**
-     * Constructeur de la vue du poisson.
+     * Constructeur de la vue du jeu.
      * 1 : Création du poisson à partir des deux images disponible en ressource.
      * 2 : Création du fond d'écran à partir de l'image background disponible en ressource.
      * 3 : Création de l'objet Paint pour la couleur du score.
      * 4 : Initialisation de la vie avec les images de coeur des ressources.
      * 5 : Initialisation de la valeur de position Y de départ du poisson
+     * 6 : Propriétés des boules jaunes.
+     * 7 : Propriétés des boules vertes
+     * 7.1 : Propriétés des boules vertes
+     * 8 : Initialisation du score à 0.
      *
      * @param context Le contexte dans lequel la vue du poisson sera.
      */
@@ -69,6 +89,21 @@ public class FlyingFishView extends View {
         //5 : Initialisation de la valeur de position Y de départ du poisson
         fishY = 550;
 
+        //6 : Propriétés des boules jaunes
+        yellowPaint.setColor(Color.YELLOW);
+        yellowPaint.setAntiAlias(false);
+
+        //7 : Propriétés des boules vertes
+        greenPaint.setColor(Color.GREEN);
+        greenPaint.setAntiAlias(false);
+
+        //7.1 : Propriétés des boules vertes
+        redPaint.setColor(Color.RED);
+        redPaint.setAntiAlias(false);
+
+        //8 : Initialisation du score à 0
+        score = 0;
+
     }
 
     /**
@@ -77,6 +112,9 @@ public class FlyingFishView extends View {
      * 1 : le fond d'écran
      * 1.1 : Initialisation de la position Y minimum et maximum du poisson
      * 2 : Action lorsque le joueur touche l'ecran
+     * 2.1 : Paramétrage des boules jaunes
+     * 2.2 : Paramétrage des boules vertes
+     * 2.3 : Paramétrage des boules rouges
      * 3 : le texte pour le score
      * 4 : Les trois coeurs de vie placés en valeurs absolues pour le moment
      *
@@ -115,18 +153,73 @@ public class FlyingFishView extends View {
             canvas.drawBitmap(fish[0], fishX, fishY, null);
         }
 
+        //2.1 : Paramétrage des boules jaunes
+        yellowX = yellowX - yellowSpeed;
+
+        if(hitBallChecker(yellowX, yellowY)){
+            score = score + 10;
+            yellowX = -100;
+        }
+
+        if (yellowX < 0) {
+            yellowX = canvasWidth + 21;
+            yellowY = (int) Math.floor(Math.random() * (maxFishY - minFishY)) + minFishY;
+        }
+        canvas.drawCircle(yellowX, yellowY, 25, yellowPaint);
+
+        //2.2 : Paramétrage des boules vertes
+        greenX = greenX - greenSpeed;
+
+        if(hitBallChecker(greenX, greenY)){
+            score = score + 20;
+            greenX = -100;
+        }
+
+        if (greenX < 0) {
+            greenX = canvasWidth + 21;
+            greenY = (int) Math.floor(Math.random() * (maxFishY - minFishY)) + minFishY;
+        }
+        canvas.drawCircle(greenX, greenY, 25, greenPaint);
+
+        //2.3 : Paramétrage des boules rouges
+        redX = redX - redSpeed;
+
+        if(hitBallChecker(redX, redY)){
+            redX = -100;
+        }
+
+        if (redX < 0) {
+            redX = canvasWidth + 21;
+            redY = (int) Math.floor(Math.random() * (maxFishY - minFishY)) + minFishY;
+        }
+        canvas.drawCircle(redX, redY, 30, redPaint);
+
         //3 : le texte pour le score
-        canvas.drawText("Score : ", 20, 60, scorePaint);
+        canvas.drawText("Score : " + score, 20, 60, scorePaint);
 
         //4 : Les trois coeurs de vie placés en valeurs absolues pour le moment
-        canvas.drawBitmap(life[0], 400, 10, null);
         canvas.drawBitmap(life[0], 475, 10, null);
         canvas.drawBitmap(life[0], 550, 10, null);
+        canvas.drawBitmap(life[0], 625, 10, null);
 
     }
 
     /**
-     * Evenement lorsque le joueur touche l'écran
+     * Methode permettant de contrôler si une boule touche le poisson
+     *
+     * @param x position X de l'élement à tester par rapport à l'emplacement du poisson
+     * @param y position Y de l'élement à tester par rapport à l'emplacement du poisson
+     * @return vrai si touché, faux si pas touché
+     */
+    public boolean hitBallChecker(int x, int y) {
+        if (fishX < x && x < (fishX + fish[0].getWidth()) && fishY < y && y < (fishY + fish[0].getHeight())) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Evenement lorsque le joueur touche l'écran permettant de faire remonter le poisson
      *
      * @param event
      * @return
