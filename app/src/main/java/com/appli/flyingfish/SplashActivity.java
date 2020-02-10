60034af0d7fd391fe461b48d6935571cefc74d71
vdelll <valentin.delaunay1@gmail.com>
2020-02-10 20:58:22 +0100

    Passage sur l'activité Main après 5 secondes de Splash Screen

--- app/src/main/java/com/appli/flyingfish/SplashActivity.java
+++ app/src/main/java/com/appli/flyingfish/SplashActivity.java
@@ -2,6 +2,7 @@ package com.appli.flyingfish;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
@@ -10,5 +11,40 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        init();
+    }
+
+    /**
+     * Initialisation du splash screen avec un thread de 5 secondes suivi du
+     * passage sur l'activité principale
+     */
+    private void init() {
+
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try{
+                    sleep(5000);
+                } catch (Exception e){
+                    e.printStackTrace();
+                } finally {
+                    Intent goToMain = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(goToMain);
+                }
+            }
+        };
+
+        thread.start();
+
+    }
+
+    /**
+     * Ferme l'activité splach une fois l'activité main lancée
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 }
